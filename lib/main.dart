f51022ae64764112e65a5001986e2e59d4cc29a5
dilinamewan <dilinamewan07@gmail.com>
2025-02-02 11:37:44 +0530

    Test2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dilina Demo',
+      title: 'Pulse Medical Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
