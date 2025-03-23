ab3541ee924ebf493fec938a53ab8e94af9bebd2
tdulshan3 <tdulshan3@gmail.com>
2025-03-23 21:25:18 +0530

    setup supabase for save documents

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'ui/signing_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +9,16 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+
   try {
     //firebase configuration
     await Firebase.initializeApp();
     //.env configuration
     await dotenv.load(fileName: ".env");
+    await Supabase.initialize(
+      url: "https://wpqjyumngwfovncafewt.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwcWp5dW1uZ3dmb3ZuY2FmZXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMDI5OTYsImV4cCI6MjA1Nzg3ODk5Nn0.E8aEzKCi3UC-8vMzXazFea_QAd0obgGfPKXVjGpaMpg",
+    );
   } catch (e) {
     throw Exception('Error $e');
   }
@@ -30,7 +37,7 @@ Future main() async {
   
   runApp(MyApp());
 }
-
+final supabase = Supabase.instance.client;
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
